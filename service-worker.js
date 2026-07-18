d3a1d2d3a65c2646996df3bbfce12a4dcbaa2c11
dal1312 <127267166+dal1312@users.noreply.github.com>
2026-07-18 07:06:55 +0200

    Redesign home with real restaurant assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,26 @@
-const CACHE_NAME = "ponte6-v8";
-const APP_SHELL = ["./","./index.html","./menu.html","./ordina.html","./ordina-rapido.html","./contatti.html","./offline.html","./css/styles.css","./js/main.js","./js/menu-data.js","./js/beverage-data.js","./manifest.json"];
+const CACHE_NAME = "ponte6-v9";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./menu.html",
+  "./ordina.html",
+  "./ordina-rapido.html",
+  "./contatti.html",
+  "./offline.html",
+  "./css/styles.css",
+  "./js/main.js",
+  "./js/menu-data.js",
+  "./js/beverage-data.js",
+  "./manifest.json",
+  "./assets/home/logo.png",
+  "./assets/home/sala.jpg",
+  "./assets/home/esterno.jpg",
+  "./assets/home/pasta-fresca.jpg",
+  "./assets/home/tartare.jpg",
+  "./assets/home/tortelli.jpg",
+  "./assets/home/pizza.jpg",
+  "./assets/home/bancone.jpg"
+];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener("fetch", event => {
