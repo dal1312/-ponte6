fb04f31a52177cf83fbc4c02568c0f603535ef0f
dal1312 <127267166+dal1312@users.noreply.github.com>
2026-07-18 05:43:46 +0200

    Refresh PWA cache for order navigation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ponte6-v3";
+const CACHE_NAME = "ponte6-v4";
 const APP_SHELL = ["./","./index.html","./menu.html","./ordina.html","./ordina-rapido.html","./contatti.html","./offline.html","./css/styles.css","./js/main.js","./js/menu-data.js","./manifest.json"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
